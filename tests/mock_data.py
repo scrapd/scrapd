631b64227dfca2c814f0dd2dd69a86e562788043
mengler <mengler@duosecurity.com>
2019-03-09 09:55:21 -0600

    Added spaces present in twitter description.

--- tests/mock_data.py
+++ tests/mock_data.py
@@ -3,9 +3,9 @@
 twitter_title_00 = "Traffic Fatality #73"
 twitter_description_00 = """
 Case:           18-3640187 Date:            December 30, 2018 Time:            2:24 a.m. Location:     1400 E. Highway
-71 eastbound Deceased:   Corbin Sabillon-Garcia, White male, DOB 02/09/80   The preliminary investigation shows that a
-2003 Ford F150 was traveling northbound on the US Highway 183 northbound ramp to E. Highway 71, eastbound. The truck
-went across the E. Highway 71 and US Highway 183 ramp, rolled and came to a stop north of the roadway.
+ 71 eastbound Deceased:   Corbin Sabillon-Garcia, White male, DOB 02/09/80   The preliminary investigation shows that a
+ 2003 Ford F150 was traveling northbound on the US Highway 183 northbound ramp to E. Highway 71, eastbound. The truck
+ went across the E. Highway 71 and US Highway 183 ramp, rolled and came to a stop north of the roadway.
 """
 twitter_description_01 = "Case:           19-0161105"
 twitter_description_02 = "Case:            18-160882 Date:             Tuesday, January 16, 2018 Time:             5:14 p.m. Location:      1500 W. Slaughter Lane Deceased:     Eva Marie Gonzales, W/F, DOB: 01-22-1961 (passenger)"
