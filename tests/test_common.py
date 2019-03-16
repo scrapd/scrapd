87dbaa176d355f7cf6eb430be9db6f80fb93e0d1
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-03-16 10:38:46 -0500

    Clean the house a little
    * Update the Changelog. Use PRs instead of issues.
    * Use test scenarios to reduce the number of test functions.
    * Use `pytest` instead of `py.test` to run the tests.

--- tests/test_common.py
+++ tests/test_common.py
@@ -3,3 +3,13 @@ from pathlib import Path
 
 TEST_ROOT_DIR = Path(__file__).resolve().parent
 TEST_DATA_DIR = TEST_ROOT_DIR / 'data'
+
+
+def scenario_inputs(scenarios):
+    """Parse the scenarios and feed the data to the test function."""
+    return [test_input[0] for test_input in scenarios]
+
+
+def scenario_ids(scenarios):
+    """Parse the scenarios and feed the IDs to the test function."""
+    return [test_input[1] for test_input in scenarios]
